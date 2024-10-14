aa68269209e0cd992cc66e85663987dd07551fb1
Thibault <49438882+Thibault-Cart@users.noreply.github.com>
2024-10-14 19:35:25 +0900

    added lifetime for bullet and some minor change

--- Assets/script/BulletForward.cs
+++ Assets/script/BulletForward.cs
@@ -7,10 +7,12 @@ public class BulletForward : MonoBehaviour
 
     public float speed = 50.0f;
     public float lifeTime = 10.0f;
+
+    private float timer = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,5 +20,14 @@ public class BulletForward : MonoBehaviour
     {
         print("avance");
         transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        timer += Time.deltaTime;
+
+        if (timer >= 10.0f)
+        {
+            Destroy(this.gameObject);
+            timer = lifeTime;
+        }
+
     }
 }
