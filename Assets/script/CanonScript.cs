cbc1e8a53b341f555a4c28cb5688dd2ff4005e7b
Thibault <49438882+Thibault-Cart@users.noreply.github.com>
2024-12-09 14:47:02 +0900

    Revert "fix: mobile button size"
    This reverts commit b654135e18033bf699e501a41f62860524a80780, reversing
    changes made to 742f7729bfa85ebf08736e42a899f3240728c7ac.

--- Assets/script/CanonScript.cs
+++ Assets/script/CanonScript.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 
 public class CanonScript : MonoBehaviour
 {
@@ -10,13 +9,11 @@ public class CanonScript : MonoBehaviour
     public GameObject bullet;
     public float shootingIntterval = 10f;
     private float timer = 0f;
-    private AudioSource boomsound;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        boomsound = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -27,7 +24,7 @@ public class CanonScript : MonoBehaviour
         if (timer >= shootingIntterval) // if ten second passed
         {
             // Action à effectuer toutes les 10 secondes
-            boomsound.Play();
+           
             Instantiate(bullet, ShootPoint.transform.position, ShootPoint.transform.rotation, transform);
             timer = 0f; // reinitilize
         }
